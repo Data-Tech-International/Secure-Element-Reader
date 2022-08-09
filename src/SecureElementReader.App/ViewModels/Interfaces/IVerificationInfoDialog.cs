9ed8496d2ca788728265353065594ad78bede04d
MarioCvitkovac <mario.cvitkovac@dti.rs>
2022-08-09 11:55:55 +0200

    VerifyInfoDialogUpdate

--- src/SecureElementReader.App/ViewModels/Interfaces/IVerificationInfoDialog.cs
+++ src/SecureElementReader.App/ViewModels/Interfaces/IVerificationInfoDialog.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace SecureElementReader.App.ViewModels.Interfaces
 {
@@ -10,5 +11,7 @@ namespace SecureElementReader.App.ViewModels.Interfaces
     {
         string VerificationDetails { get; set; }
         string TapUrl { get; set; }
+
+        public ICommand GoToTAP { get; set; }
     }
 }
--- src/SecureElementReader.App/ViewModels/VerificationInfoDialogViewModel.cs
+++ src/SecureElementReader.App/ViewModels/VerificationInfoDialogViewModel.cs
@@ -1,10 +1,13 @@
-﻿using SecureElementReader.App.ViewModels.Implementations.Dialogs;
+﻿using ReactiveUI;
+using SecureElementReader.App.ViewModels.Implementations.Dialogs;
 using SecureElementReader.App.ViewModels.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace SecureElementReader.App.ViewModels
 {
@@ -12,6 +15,12 @@ namespace SecureElementReader.App.ViewModels
     {
         public string VerificationDetails { get; set; }
         public string TapUrl { get; set; }
+
+        public ICommand CloseButton { get; }
+
+        public ICommand GoToTAP { get; set; }
+
+        
 
         public ICertDetailsViewModel CertDetailsViewModel { get; }
 
@@ -21,10 +30,28 @@ namespace SecureElementReader.App.ViewModels
             PopulateDialog();            
         }
 
+       
         private void PopulateDialog()
         {            
             VerificationDetails = CertDetailsViewModel?.CertDetailsModel?.PKIVerificationInfo;
             TapUrl = CertDetailsViewModel?.CertDetailsModel?.ApiUrl.Replace("api","tap");
+            GoToTAP = ReactiveCommand.Create(OnButtonTAP);
+        }
+
+        public VerificationInfoDialogViewModel()
+        {
+            CloseButton = ReactiveCommand.Create(ButtonClose);
+        }
+
+        private void ButtonClose()
+        {
+            Close();
+        }
+
+        private void OnButtonTAP()
+        {
+            Process.Start(new ProcessStartInfo("https://tap.sandbox.suf.purs.gov.rs/Help/view/1048069196/Инсталирање-RCA-и-ICA-сертификата/sr-Cyrl-RS") { UseShellExecute = true });
+
         }
     }
 }
